cc4a67732edadf4212af64794dd3100cc0dfdbd8
Mawuawoe <desmonddzakago@gmail.com>
2024-11-14 14:51:55 +0000

    task_6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module to implement basic Authentication
+"""
+from flask import request
+from typing import TypeVar, List
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    
+    """
+    pass
