add9b674b25c312a2fd6f1d5d9d7dfd72c0a29db
Mawuawoe <desmonddzakago@gmail.com>
2024-11-14 11:45:17 +0000

    task_4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,22 @@ class Auth():
     """the class to handle basic authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """access required"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # Ensure path and excluded paths are slash-tolerant
+        normalized_path = path if path.endswith('/') else path + '/'
+
+        for ex_path in excluded_paths:
+            if ex_path.endswith('/'):
+                if normalized_path == ex_path:
+                    return False
+            else:
+                if normalized_path == ex_path + '/':
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization_header"""
