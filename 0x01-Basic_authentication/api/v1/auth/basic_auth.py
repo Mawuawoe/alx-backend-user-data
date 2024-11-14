1f4eeec8527d0c12158199588bfa24b17ade900a
Mawuawoe <desmonddzakago@gmail.com>
2024-11-14 15:12:51 +0000

    task_7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,11 +4,24 @@ module to implement basic Authentication
 """
 from flask import request
 from typing import TypeVar, List
-from api.v1.auth.auth import Auth
+# from api.v1.auth.auth import Auth
+from auth import Auth
 
 
 class BasicAuth(Auth):
     """
     class to implement basic Authentication
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        to extract the Base64 encoded string
+        for basic authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ")[1]
