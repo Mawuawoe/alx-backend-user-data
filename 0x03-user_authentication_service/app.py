651c260730b493e7363c164f9681d06757f91778
Mawuawoe <desmonddzakago@gmail.com>
2024-11-20 22:35:59 +0000

    task_6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+simple flask app
+"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    GET method that return
+    a dict
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
