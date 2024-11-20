18b93d8a4f1adf4d0fddc1e747e4a50de6f1ae23
Mawuawoe <desmonddzakago@gmail.com>
2024-11-20 19:08:58 +0000

    task_0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 module that defines the users table
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
